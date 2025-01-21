ced97fff7d0dd2c2ae86315003cc69ccd52d4eec
Sachin1234564646 <sachin1garhwal@gmail.com>
2025-01-21 20:14:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Ssmovies_latest')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SS_MoviesChannel')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Ssmovies_latest') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Ssmovies_latest') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/boss_5613')
 
 # True Or False
