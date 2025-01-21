9637e2e6e68139ee29293ab7c5d6fb2a3884bd3b
Sachin1234564646 <sachin1garhwal@gmail.com>
2025-01-22 00:26:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -128,7 +128,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'yamlinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '4adb2ba8f7b0ad1f295f4278f5a756b8f5e9dcab')
-TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/SS_MoviesChannel/2') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
